2ee11620c22636fb9e6ff41489ae181438fc0c75
Artem Bodunov <artembodunov@mail.ru>
2020-08-03 22:09:40 +0300

    add linreg2d

--- src/Linreg.hs
+++ src/Linreg.hs
@@ -2,26 +2,27 @@ module Linreg where
 
 import           SGD
 
-a = [1..100] :: [Double]
-b = map (+2)$ map (*5) a
+a = [1..50] :: [Double]
+b = map (subtract 2 . (* 5)) a
 
+x = [1..10] :: [Double]
+y = reverse [-10..(-1)] :: [Double]
 
-{-
-    Y_pred = m*X + c  # The current predicted value of Y
-    D_m = (-2/n) * sum(X * (Y - Y_pred))  # Derivative wrt m
-    D_c = (-2/n) * sum(Y - Y_pred)  # Derivative wrt c
-    m = m - L * D_m  # Update m
-    c = c - L * D_c  # Update c
- - -}
-
-linReg :: [Double] -> [Double] -> Double -> Double -> Integer -> (Double, Double)
-linReg x y m c epochs
-    | epochs == 0 =  (m,c)
-    | otherwise = linReg  x y newM newC (epochs - 1)
+linReg2d :: [Double] -> [Double] -> Double -> Double -> Integer -> (Double, Double)
+linReg2d x y w b epochs
+    | epochs == 0 = (w,b)
+    | otherwise = linReg2d x y newW newB (epochs - 1)
     where
-        yPred = map (+c) $ map (*m) x
-        dy = zipWith (-) y yPred
-        dm = - (sum (zipWith (*) x dy))
-        dc = - (sum dy)
-        newM = m - eta * dm
-        newC = c - eta * dc
+        n = fromIntegral $ length x
+        -- d/dw(Loss) = -2 * x_i * (y_i - (w * x_i + b))
+        dw' = map ((+b) . (*w)) x
+        dw'' = sum $ zipWith (*) x (zipWith (-) y dw')
+        dw = (-2) *  dw''
+
+        -- d/db(Loss) = -2 * (y_i - (w * x_i + b))
+        db' = map ((+b) . (*w)) x
+        db'' = sum $ zipWith (-) y db'
+        db = (-2) * db''
+
+        newW = w - eta * dw / n
+        newB = b - eta * db / n
--- src/SGD.hs
+++ src/SGD.hs
@@ -4,10 +4,13 @@ import           Data.Optional
 eps = 1e-5 :: Double
 eta = 1e-3 :: Double
 
+
+-- e.g. diff sin 0.5 -> 0.479425538604203
 diff :: (Double -> Double) -> Double ->Double
 diff f x = (f (x + eps) - f (x - eps) )/ (2 * eps)
 
-
+-- e.g. gdStep sin 1 (<- starting point x_0) 
+-- -> 0.999459697694143 (next approximation x_n+1)
 gdStep :: (Double -> Double) -> Double -> Double
 gdStep f x = x - eta * diff f x
 
@@ -17,6 +20,6 @@ gdFindMin' f x n
       | n <= 0    = x
       | otherwise = gdFindMin'  f (gdStep f x) (n-1)
 
-
+-- gdFindMin f x_0 (numSteps = 10000)
 gdFindMin :: (Double ->Double) -> Double -> Double
 gdFindMin f x = gdFindMin' f x 10000
